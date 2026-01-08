28c5368d2503df7120e4182748171fcaadfc823a
Christiaan Du Toit <christiaandutoit2006@gmail.com>
2026-01-09 00:38:31 +0200

    Added my D&D dice roller and MicrosoftLearn Exercises

---
+++ Microsoft-Learn/Microsoft_Learn_GPA_Calculator/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microsoft_Learn_GPA_Calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int grade1 = 4;
+            int grade2 = 3;
+            int grade3 = 3;
+            int grade4 = 3;
+            int grade5 = 4;
+
+            int gradeTotal = grade1 + grade2 + grade3 + grade4 + grade5;
+
+            int credit1 = 3;
+            int credit2 = 3;
+            int credit3 = 4;
+            int credit4 = 4;
+            int credit5 = 3;
+
+            int creditTotal = credit1 + credit2 + credit3 + credit4 + credit5;
+
+            int totalGpa = grade1 * credit1 + grade2 * credit2 + grade3 * credit3 + grade4 * credit4 + grade5 * credit5;
+
+            double gpaOne = (double) totalGpa/creditTotal;
+            
+            double gpaFinal = Math.Round(gpaOne, 2); 
+
+            Console.WriteLine("Student: Sophia Johnson");
+
+            Console.WriteLine("");
+
+            Console.WriteLine("Course\t\tGrade\t\tCredit Hours");
+            Console.WriteLine($"English 101\t\t{grade1}\t\t{credit1}");
+            Console.WriteLine($"Algebra 101\t\t{grade2}\t\t{credit2}");
+            Console.WriteLine($"Biology 101\t\t{grade3}\t\t{credit3}");
+            Console.WriteLine($"Computer Science I\t{grade4}\t\t{credit4}");
+            Console.WriteLine($"Psychology 101\t\t{grade5}\t\t{credit5}");
+
+            Console.WriteLine("");
+
+            Console.WriteLine($"Final GPA: \t\t{gpaFinal}");
+
+        }
+    }
+}
---
+++ Personal-Projects/DiceRoller(D&D)/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiceRoller_D_D_
+{
+    internal class Program
+    {
+        enum Dices
+        {
+            D4 = 1,
+            D6,
+            D8,
+            D10,
+            D12,
+            D20,
+            D100,
+            Exit
+        }
+
+        static void Main(string[] args)
+        {
+            Random dice = new Random();
+            int d4 = dice.Next(1, 5);
+            int d6 = dice.Next(1, 7);
+            int d8 = dice.Next(1, 9);
+            int d10 = dice.Next(1, 11);
+            int d12 = dice.Next(1, 13);
+            int d20 = dice.Next(1, 21);
+            int d100 = dice.Next(1, 101);
+
+            bool running = true;
+
+            while (running)
+            {
+                for (int i = 1; i <= Enum.GetValues(typeof(Dices)).Length; i++)
+                {
+                    Console.WriteLine($"{i}. {Enum.GetName(typeof(Dices), i)}");
+                }
+
+                int choices = int.Parse(Console.ReadLine());
+
+                switch (choices)
+                {
+                    case 1:
+                        Console.WriteLine($"Your roll is: {d4}");
+                    break;
+
+                    case 2:
+                        Console.WriteLine($"Your roll is: {d6}");
+                    break;
+
+                    case 3:
+                        Console.WriteLine($"Your roll is: {d8}");
+                    break;
+
+                    case 4:
+                        Console.WriteLine($"Your roll is: {d10}");
+                    break;
+
+                    case 5:
+                        Console.WriteLine($"Your roll is: {d12}");
+                    break;
+
+                    case 6:
+                        Console.WriteLine($"Your roll is: {d20}");
+                    break;
+
+                    case 7:
+                        Console.WriteLine($"Your roll is: {d100}");
+                        break;
+
+                    case 8:
+                        running = false;
+                    break;
+                }
+
+                Console.ReadLine();
+                Console.Clear();
+            }
+        }
+    }
+}
