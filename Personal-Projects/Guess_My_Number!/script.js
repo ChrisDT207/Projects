df1d51a4e5c813ae3ce2263e91de1e99ffffca4b
Christiaan Du Toit <christiaandutoit2006@gmail.com>
2026-04-26 14:27:12 +0200

    Added more JavaScript to the GMN

--- Personal-Projects/Guess_My_Number!/script.js
+++ Personal-Projects/Guess_My_Number!/script.js
@@ -1,7 +1,7 @@
 'use strict';
 
-const number = Math.trunc(Math.random() * 20)+1;
-document.querySelector('.number').textContent = number
+let number = Math.trunc(Math.random() * 20)+1;
+let score = 20;
 
 document.querySelector('.check').addEventListener('click', function() 
 {
@@ -14,14 +14,49 @@ document.querySelector('.check').addEventListener('click', function()
     }
     else if (guess === number) 
     {
+        document.querySelector('.number').textContent = number
         document.querySelector('.message').textContent = '🎉 Correct Number!'; 
+        document.querySelector('body').style.backgroundColor = 'green';
+        document.querySelector('.number').style.width = '30rem';
     }
     else if (guess > number) 
     {
-        document.querySelector('.message').textContent = '📈Too high!';    
+        if (score > 1) 
+        {
+            document.querySelector('.message').textContent = '📈Too high!';  
+            score--; 
+            document.querySelector('.score').textContent = score;    
+        }
+        else
+        {
+            document.querySelector('.message').textContent = '💥 You lost the game!'
+            document.querySelector('.score').textContent = 0
+        }
     }
     else if (guess < number) 
     {
-        document.querySelector('.message').textContent = '📉 Too low!'    
+        if (score > 1) 
+        {            
+            document.querySelector('.message').textContent = '📉 Too low!';    
+            score--;
+            document.querySelector('.score').textContent = score;    
+        }
+        else
+        {
+            document.querySelector('.message').textContent = '💥 You lost the game!'
+            document.querySelector('.score').textContent = 0            
+        }
     }
+});
+
+document.querySelector('.again').addEventListener('click', function () 
+{
+    score = 20;
+    let number = Math.trunc(Math.random() * 20)+1;
+    document.querySelector('.message').textContent = 'Start guessing...';
+    document.querySelector('.score').textContent = score;
+    document.querySelector('.number').textContent = '?';
+    document.querySelector('.number').style.width = '15rem'
+    document.querySelector('.guess').value = '';
+    document.querySelector('body').style.backgroundColor = '#222'
 });
